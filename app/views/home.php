ff239b5b5488bd17d7d72067f0da9c314f29cfe7
Ascendings <brotherballantine@gmail.com>
2015-11-27 12:04:13 -0500

    Changed homepage to match Bootstrap styles

--- app/views/home.php
+++ app/views/home.php
@@ -2,24 +2,23 @@
 
 {% block content %}
 	<div class="row">
-		<div class="medium-6 medium-offset-3 columns">
-			<div class="panel">
-				<form action="#" method="POST" autocomplete="off">
-					<div class="input-group">
-						<label for="email">
-							Send to
+		<div class="col-md-6 col-md-offset-3">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<form action="#" method="POST" autocomplete="off">
+						<div class="input-group">
+							<label for="email">Send to</label>
 							<input type="email" name="email" id="email" placeholder="someone@example.com" />
-						</label>
-					</div>
-					<div class="input-group">
-						<label for="message">
-							Message
+						</div>
+						
+						<div class="input-group">
+							<label for="message">Message</label>
 							<textarea name="message" id="message" placeholder="Enter a message to your recipient" cols="30" rows="10"></textarea>
-						</label>
-					</div>
-					
-					<button class="btn">Send</button>
-				</form>
+						</div>
+						
+						<button class="btn">Send</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
