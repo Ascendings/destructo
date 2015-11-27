11e3cb78753298a173ed06d41e1eca23d1a18bd5
Ascendings <brotherballantine@gmail.com>
2015-11-27 11:33:37 -0500

    Moved config and resources outside of app folder

---
+++ config/example.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	
+	'url' => 'https://destructo.example.com',
+	
+	'db' => [
+		'mysql' => [
+			'host' => 'db.example.com',
+			'dbname' => 'example',
+			'username' => 'example',
+			'password' => 'examplePass',
+		]
+	],
+	
+];
