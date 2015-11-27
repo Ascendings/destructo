c0e6fe6558c3bae0863f357114d3a1e9c1842669
Ascendings <brotherballantine@gmail.com>
2015-11-27 12:22:42 -0500

    Added form-control class to input fields

--- app/views/home.php
+++ app/views/home.php
@@ -8,12 +8,12 @@
 					<form action="#" method="POST" autocomplete="off">
 						<div class="form-group">
 							<label for="email">Send to</label>
-							<input type="email" name="email" id="email" placeholder="someone@example.com" />
+							<input type="email" name="email" id="email" class="form-control" placeholder="someone@example.com" />
 						</div>
 						
 						<div class="form-group">
 							<label for="message">Message</label>
-							<textarea name="message" id="message" placeholder="Enter a message to your recipient" cols="30" rows="10"></textarea>
+							<textarea name="message" id="message" placeholder="Enter a message to your recipient" cols="30" rows="10"  class="form-control"></textarea>
 						</div>
 						
 						<button class="btn">Send</button>
