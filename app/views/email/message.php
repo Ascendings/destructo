bd30ecbab8584f9abbc2f3058d8c4232a964c713
Ascendings <brotherballantine@gmail.com>
2015-11-27 15:05:20 -0500

    Added the send message email view

---
+++ app/views/email/message.php
@@ -0,0 +1,6 @@
+<p>You have a self destructing message from Destructo!</p><br />
+
+<p><a href="{{ base_url() }}{{ path_for('message', {'hash': hash}) }}">{{ base_url() }}{{ path_for('message', {'hash': hash}) }}</a></p><br />
+
+<p>Thanks,</p>
+<p>Destructo - by Field Protocol</p>
