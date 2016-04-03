92aec610ad4035644d16fa9e339d07babcdf0bf9
gballan1 <brotherballantine@gmail.com>
2016-04-03 09:54:42 -0400

    Added navbar to the app

--- app/views/templates/default.php
+++ app/views/templates/default.php
@@ -5,13 +5,17 @@
 	<Title>Home</title>
 	<!-- stylesheets -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<!-- javascripts -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
 <body>
 	<div class="container">
+		{% include 'templates/partials/navbar.twig' %}
+
 		{% block content %}{% endblock %}
 	</div>
+
+	<!-- javascripts -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
