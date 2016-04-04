cf32c0ec8a2cee7c19c7304e6587d53a1617e47f
Ascendings <brotherballantine@gmail.com>
2016-04-04 16:47:07 -0400

    Moved some views around

--- app/routes.php
+++ app/routes.php
@@ -68,3 +68,10 @@ $app->get('/message/{hash}', function($request, $response, $args) {
 	]);
 
 })->setName('show');
+
+// send a message
+$app->get('/message/new', function($request, $response, $args) {
+
+  $this->view->render($response, 'message/new.twig');
+
+})->setName('message.new');
